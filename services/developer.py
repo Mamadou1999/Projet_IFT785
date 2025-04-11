fd4dbc10f034ad11d1f1f47288a562595a4e4993
abdukader <seckader.pro@gmail.com>
2025-04-11 10:39:47 -0400

    test: Ajout de la classe de test test_developer.py

--- services/developer.py
+++ services/developer.py
@@ -1,6 +1,6 @@
-from user import User
-from publicprofilstrategy import PublicProfileStrategy
-from privateprofilstrategy import PrivateProfileStrategy
+from services.user import User
+from services.publicprofilstrategy import PublicProfileStrategy
+from services.privateprofilstrategy import PrivateProfileStrategy
 
 
 # Classe Developer qui hérite de User
@@ -13,9 +13,12 @@ class Developer(User):
         self.biography = ""
         self.avatar = ""
         self.location = ""
-        self.is_profile_public = True
+        self.profile_strategy = None
     
     def show_profile(self):
         print(f"Affichage du profil développeur de {self.name}")
         strategy = PublicProfileStrategy() if self.is_profile_public else PrivateProfileStrategy()
         strategy.display_profile(self)
+
+    def set_profile_strategy(self, strategy):
+        self.profile_strategy = strategy
