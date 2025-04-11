d6401ecce90db0e66e7c10e596ab7ff195b5e2ca
abdukader <seckader.pro@gmail.com>
2025-04-11 10:42:47 -0400

    test: Ajout de la classe de test test_user_observer.py

--- services/jobmatchobserver.py
+++ services/jobmatchobserver.py
@@ -1,5 +1,6 @@
-from userobserver import UserObserver
+from services.userobserver import UserObserver
 # Classe concrète JobMatchObserver
 class JobMatchObserver(UserObserver):
+
     def update(self, user, event):
         print(f"JobMatchObserver: Mise à jour pour l'utilisateur {user.name} avec l'événement '{event}'")
