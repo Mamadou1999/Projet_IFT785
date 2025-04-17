7f1c2c682a5086fce0736e6b21bd68223dec0c2c
Mamadou1999 <diopmamadou706@gmail.com>
2025-04-16 21:42:54 -0400

    refactor:modifier les attributs de la classe Company

--- services/company.py
+++ services/company.py
@@ -1,17 +1,28 @@
 from services.user import User
+from services.jobposting import JobPosting
 
 # Classe Company qui hérite de User
 class Company(User):
     def __init__(self):
         super().__init__()
-        self.company_name = ""
-        self.description = ""
-        self.location = ""
-        self.logo = ""
+        self._company_name = ""
+        self._description = ""
+        self._location = ""
+        self._logo = ""
     
-    def create_job_offer(self):
-        print(f"Création d'une offre d'emploi par {self.company_name}")
-        return "job_offer"
+    def create_job_offer(self, title, location, technologies, experience_level, salary_range, description):
+
+        job = JobPosting()
+
+        job.title = title
+        job.location = location
+        job.technologies = technologies
+        job.experience_level = experience_level
+        job.salary_range = salary_range
+        job.description = description
+        job.company = self
+
+        return job
     
     def view_developer_profiles(self):
         print(f"Visualisation des profils de développeurs par {self.company_name}")
